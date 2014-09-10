be50927341f58183ea0d44f5a5d34c779e6b7d65
Songjein <jeinsong100@gmail.com>
2014-09-10 11:09:56 +0900

    chat prog init

---
+++ db_test/apps/settings.py
@@ -0,0 +1,18 @@
+"""
+settings.py
+
+Configuration for Flask app
+
+"""
+
+
+class Config(object):
+    # Set secret key to use session
+    SECRET_KEY = "likelion-flaskr-secret-key"
+    debug = False
+
+
+class Production(Config):
+    debug = True
+    CSRF_ENABLED = False
+    ADMIN = "astyfx@gmail.com"
